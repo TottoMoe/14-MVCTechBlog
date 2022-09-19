32e00e5a678daa937e4a419b013b1bfd4972e5e7
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-09-19 12:18:58 -0700

    update seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,37 +1,40 @@
-const sequelize = require('../config/connection');
-const userData = require('./userData.json');
-const postData = require('./postData.json');
-const commentData = require('./commentData.json');
-const { User, Post, Comment } = require('../models');
+const sequelize = require("../config/connection");
+const userData = require("./userData.json");
+const postData = require("./postData.json");
+const commentData = require("./commentData.json");
+const { User, Post, Comment } = require("../models");
 
+// const seedDatabase = async () => {
+//   await sequelize.sync({ force: true });
+
+//   const users = await User.bulkCreate(userData, {
+//     individualHooks: true,
+//     returning: true,
+//   });
+
+//   for (const post of postData) {
+//     await Post.create({
+//       ...post,
+//       user_id: users[Math.floor(Math.random() * users.length)].id,
+//     });
+//   }
+
+//   for (const comment of commentData) {
+//     await Comment.create({
+//       ...comment,
+//       post_id: users[Math.floor(Math.random() * users.length)].id,
+//     });
+//   }
+
+//   process.exit(0);
+// };
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
-  
-  const users = await User.bulkCreate(userData, {
-    individualHooks: true,
-    returning: true,
-  });
-  
-  for (const post of postData) {
-    await Post.create({
-      ...post,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
-  
-  for (const comment of commentData) {
-    await Comment.create({
-      ...comment,
-      post_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
-  
+  await User.bulkCreate(userData, { individualHooks: true });
+  await Post.bulkCreate(postData);
+  await Comment.bulkCreate(commentData);
+
   process.exit(0);
 };
-// const seedDatabase = async () => {
-//   await User.bulkCreate(users, { individualHooks: true });
-//   await Post.bulkCreate(posts);
-//   await Comment.bulkCreate(comments);
-// }
 
 seedDatabase();
