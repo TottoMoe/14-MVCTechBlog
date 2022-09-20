f975095e45b95ef5a44f898fefaef1272e3b47e9
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-09-20 11:27:15 -0700

    updated login js from the public

--- public/js/login.js
+++ public/js/login.js
@@ -2,29 +2,27 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const usernameEl = document.querySelector('#username-login').value.trim();
-  const passwordEl = document.querySelector('#password-login').value.trim();
+  const usernameEl = document.querySelector("#username-input-login");
+  const passwordEl = document.querySelector("#password-input-login");
 
   // Send a POST request to the API endpoint
-  const response = await fetch('/api/users/login', {
-    method: 'POST',
-    body: JSON.stringify({ 
+  const response = await fetch("/api/users/login", {
+    method: "POST",
+    body: JSON.stringify({
       username: usernameEl.value,
       password: passwordEl.value,
     }),
-    headers: { 'Content-Type': 'application/json' },
+    headers: { "Content-Type": "application/json" },
   });
 
   if (response.ok) {
     // If successful, redirect the browser to the dashboard page
-    document.location.replace('/dashboard');
+    document.location.replace("/dashboard");
   } else {
     alert(response.statusText);
   }
-
 };
 
 document
-  .querySelector('#login-form')
-  .addEventListener('submit', loginFormHandler);
-
+  .querySelector("#login-form")
+  .addEventListener("submit", loginFormHandler);
